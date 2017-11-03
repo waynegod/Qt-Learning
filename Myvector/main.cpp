17c249829eebb7230f962f42f6091a22838ce382
waynegod <30386953+waynegod@users.noreply.github.com>
2017-11-03 16:26:32 +0800

    Add files via upload

--- Myvector/main.cpp
+++ Myvector/main.cpp
@@ -1,8 +1,27 @@
-#include <QCoreApplication>
+#include <QVector>
+#include <QTextStream>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+int main(void) {
 
-    return a.exec();
+    QTextStream out(stdout);
+
+    QVector<int> vals = {1, 2, 3, 4, 5,5,5}; //動態容器
+
+    out << "The size of the vector is: " << vals.size() << endl;
+
+    out << "The first item is: " << vals.first() << endl;
+    out << "The last item is: " << vals.last() << endl;
+
+    vals.append(6);//新增在最後一位
+    vals.prepend(0);//從第一位插入
+
+    out << "Elements: ";
+
+    for (int val : vals)//由vals內容長度決定執行幾次，val依vals決定
+        out << val << " ";
+
+
+    out << endl;
+
+    return 0;
 }
