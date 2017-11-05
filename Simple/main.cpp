965cbcb2e4481be9d858349a56f96a0575604ebf
waynegod <30386953+waynegod@users.noreply.github.com>
2017-11-05 20:01:26 +0800

    Add files via upload

--- Simple/main.cpp
+++ Simple/main.cpp
@@ -8,7 +8,9 @@ int main(int argc, char *argv[]) {
     QWidget window;
 
     window.resize(250, 150);//視窗大小
+    window.move(300, 300);//視窗位置
     window.setWindowTitle("Simple example");//標頭名稱
+    window.setToolTip("QWidget");//游標停駐時顯示的提示
     window.show();//顯示視窗
 
     return app.exec();//回傳程式的執行迴圈
